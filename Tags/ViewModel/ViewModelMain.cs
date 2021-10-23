1cc1b0d29cb433e2800cb48ad9bd79a54e63c9c8
kachkov <usemuse@mail.ru>
2021-10-23 23:52:17 +0300

    Добавьте файлы проекта.

---
+++ Tags/ViewModel/ViewModelMain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Tags.ViewModel
+{
+    public class ViewModelMain : BaseInpc
+    {
+        private ObservableCollection<ViewModelTag> _tags;
+        public ObservableCollection<ViewModelTag> Tags
+        {
+            get => _tags;
+            private set => Set(ref _tags, value);
+        }
+
+        private ObservableCollection<ViewModelTag> _selectedTags;
+        public ObservableCollection<ViewModelTag> SelectedTags
+        {
+            get => _selectedTags;
+            private set => Set(ref _selectedTags, value);
+        }
+
+        public ViewModelMain()
+        {
+            var t1 = new ViewModelTag("Тег 1");
+            var t2 = new ViewModelTag("Тег 2");
+            var t3 = new ViewModelTag("Тег 3");
+            var tags = new List<ViewModelTag>() { t1, t2, t3 };
+
+            Tags = new ObservableCollection<ViewModelTag>(tags);
+            DropDownClosedCommand = new RelayCommand((object parameter) =>
+            {
+                var selectedTag = Tags.Where(t => t.IsChecked);
+                SelectedTags = new ObservableCollection<ViewModelTag>(selectedTag);
+            });
+
+
+        }
+
+        public ICommand DropDownClosedCommand { get; set; }
+    }
+}
---
+++ Tags/ViewModel/ViewModelTag.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Tags.ViewModel
+{
+    public class ViewModelTag : BaseInpc
+    {
+        private bool isChecked;
+        public bool IsChecked
+        {
+            get => isChecked;
+            set => Set(ref isChecked, value);
+        }
+
+        private string title;
+        public string Title
+        {
+            get => title;
+            set => Set(ref title, value);
+        }
+
+        public ViewModelTag(string title)
+        {
+            this.Title = title;
+
+            CheckedCommand = new RelayCommand((object parameter) =>
+                     {
+
+                     });
+            UncheckedCommand = new RelayCommand((object parameter) =>
+            {
+
+            });
+
+        }
+        public ICommand CheckedCommand { get; set; }
+        public ICommand UncheckedCommand { get; set; }
+    }
+}
